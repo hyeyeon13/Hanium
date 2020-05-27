29e80c7b6caf658e028d83d89ded7a1ba6faf1f4
hyelin97 <777gpfls@naver.com>
2020-05-27 13:47:38 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/org/techtown/hanium/MainActivity.java
+++ app/src/main/java/org/techtown/hanium/MainActivity.java
@@ -2,7 +2,10 @@ package org.techtown.hanium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button=(Button)findViewById(R.id.button8);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),PathSetting.class);
+                startActivity(intent);
+            }
+        });
     }
 }
