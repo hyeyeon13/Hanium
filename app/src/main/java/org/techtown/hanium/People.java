a0f66dee8185a71e63c9a9720ae8ba3e7ebb9b1c
lsm3379 <lsmsm307@naver.com>
2020-05-27 23:34:24 +0900

    dddd

--- app/src/main/java/org/techtown/hanium/People.java
+++ app/src/main/java/org/techtown/hanium/People.java
@@ -15,23 +15,7 @@ public class People extends AppCompatActivity {
         setContentView(R.layout.activity_people);
     }
 
-    public class MainActivity extends Activity {
 
-        private Button setcheck;
-
-        @SuppressLint("ResourceType")
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            // 레이아웃에서 버튼 id 가져오기.
-            setcheck = (Button) findViewById(R.id.gr);
-            // 해당 버튼에 레이아웃 설정하기.
-            setcheck.setBackgroundResource(R.layout.gr);
-
-        }
-    }
 
 
 
