a1c72853b1ecd54a28029ab1706055abd842a421
lsm3379 <lsmsm307@naver.com>
2020-06-08 18:46:29 +0900

    dddd

--- app/src/main/java/org/techtown/hanium/select_path.java
+++ app/src/main/java/org/techtown/hanium/select_path.java
@@ -17,10 +17,6 @@ import com.skt.Tmap.TMapView;
 import org.json.JSONException;
 
 import java.util.HashMap;
-import com.odsay.odsayandroidsdk.API;
-import com.odsay.odsayandroidsdk.ODsayData;
-import com.odsay.odsayandroidsdk.ODsayService;
-import com.odsay.odsayandroidsdk.OnResultCallbackListener;
 
 public class select_path extends AppCompatActivity {
 
@@ -30,53 +26,61 @@ public class select_path extends AppCompatActivity {
         setContentView(R.layout.activity_select_path);
         EditText editText=(EditText)findViewById(R.id.editText);
         RelativeLayout relativeLayout = new RelativeLayout(this);
+        // 싱글톤 생성, Key 값을 활용하여 객체 생성
+        ODsayService odsayService = ODsayService.init(getApplicationContext(), "o35DS9VMHDOCosWoVhEYWv43HTeN5uX6ID/cO660rlI");
+        // 서버 연결 제한 시간(단위(초), default : 5초)
+        odsayService.setReadTimeout(5000);
+        // 데이터 획득 제한 시간(단위(초), default : 5초)
+        odsayService.setConnectionTimeout(5000);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            // 싱글톤 생성, Key 값을 활용하여 객체 생성
-            ODsayService odsayService = ODsayService.init(Context, {o35DS9VMHDOCosWoVhEYWv43HTeN5uX6ID/cO660rlI});
-            // 서버 연결 제한 시간(단위(초), default : 5초)
-            odsayService.setReadTimeout(5000);
-            // 데이터 획득 제한 시간(단위(초), default : 5초)
-            odsayService.setConnectionTimeout(5000);
-
-            // 콜백 함수 구현
-            OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
-                // 호출 성공 시 실행
-                @Override
-                public void onSuccess(ODsayData odsayData, API api) {
-                    try {
-                        // API Value 는 API 호출 메소드 명을 따라갑니다.
-                        if (api == API.BUS_STATION_INFO) {
-                            String stationName = odsayData.getJson().getJSONObject("result").getString("stationName");
-                            Log.d("Station name : %s", stationName);
-                        }
-                    }catch (JSONException e) {
-                        e.printStackTrace();
+        // 콜백 함수 구현
+        OnResultCallbackListener onResultCallbackListener = new OnResultCallbackListener() {
+            // 호출 성공 시 실행
+            @Override
+            public void onSuccess(ODsayData odsayData, API api) {
+                try {
+                    // API Value 는 API 호출 메소드 명을 따라갑니다.
+                    if (api == API.BUS_STATION_INFO) {
+                        String stationName = odsayData.getJson().getJSONObject("result").getString("stationName");
+                        String x = odsayData.getJson().getJSONObject("result").getString("x");
+                        String y = odsayData.getJson().getJSONObject("result").getString("y");
+                        Log.d("Station name ", stationName);
+                        Log.d("x ", x);
+                        Log.d("y ", y);
                     }
+                }catch (JSONException e) {
+                    e.printStackTrace();
+
                 }
-                // 호출 실패 시 실행
-                @Override
-                public void onError(int i, String s, API api) {
-                    if (api == API.BUS_STATION_INFO) {}
-                }
-            };
-            // API 호출
-            odsayService.requestBusStationInfo("107475", onResultCallbackListener());
-        }
+            }
+            // 호출 실패 시 실행
+            @Override
+            public void onError(int i, String s, API api) {
+
+                if (api == API.BUS_STATION_INFO) {}
+            }
+        };
+        // API 호출
+        odsayService.requestBusStationInfo("107475", onResultCallbackListener);
+
+
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//
+//        }
 
 
 
 
-        relativeLayout.addView(tmapview);
+        //relativeLayout.addView(tmapview);
         setContentView(relativeLayout);
     }
-    OnResultCallbackListener() {
-        @Override
-        public void onSuccess(ODsayData odsayData, API api) {}
-
-        @Override
-        public void onError(int code, String message, API api) {}
-    }
+//    OnResultCallbackListener() {
+//        @Override
+//        public void onSuccess(ODsayData odsayData, API api) {}
+//
+//        @Override
+//        public void onError(int code, String message, API api) {}
+//    }
     }
 
