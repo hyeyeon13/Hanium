2656ef55d630d63f40db1f2bdce4bfa6083b1257
hyeyeon13 <60658766+hyeyeon13@users.noreply.github.com>
2020-10-17 10:14:33 +0900

    내위치, 길찾기 경로 간 거리구하기-오류

--- app/src/main/java/org/techtown/hanium/Marker.java
+++ app/src/main/java/org/techtown/hanium/Marker.java
@@ -1,13 +1,14 @@
 package org.techtown.hanium;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.DropBoxManager;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -16,31 +17,52 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.skt.Tmap.TMapCircle;
+import com.skt.Tmap.TMapGpsManager;
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
 import com.skt.Tmap.TMapTapi;
 import com.skt.Tmap.TMapView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Marker extends AppCompatActivity {
+    private boolean mTrackingMode = true;
+    private TMapView tmapview;
+
+    ArrayList<String> pathData = new ArrayList<String>();
     Double myLongitude, myLatitude;
     Double destLongitude, destLatitude;
     String stationName;
     //경도 : longitude 범위 : 127
     //위도 : latitude 범위 : 37
+    double min;
+    Double distance;
+    String[] dist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker);
 
         RelativeLayout relativeLayout = new RelativeLayout(this);
-        TMapView tmapview = new TMapView(this);
+        tmapview = new TMapView(this);
         tmapview.setSKTMapApiKey("l7xxa9511b15f91f4c3e97455a7a1ac155d2");
         tmapview.setZoomLevel(10);
         tmapview.setMapPosition(TMapView.POSITION_DEFAULT);
         TMapTapi tMapTapi = new TMapTapi(this);
+//        tmapview.setCompassMode(true);
+//        tmapgps.setMinTime(1000);
+//        tmapgps.setMinDistance(5);
+//        tmapgps.OpenGps();
+//        tmapview.setTrackingMode(true);
+//        tmapview.setSightVisible(true);
+//        tmapview.setIconVisibility(true);
+//        tmapgps.setProvider(tmapgps.GPS_PROVIDER);
+//        TMapPoint point = tmapgps.getLocation();
 
         Intent intent=getIntent();
 
@@ -50,6 +72,8 @@ public class Marker extends AppCompatActivity {
         myLatitude=intent.getExtras().getDouble("curLatitude");
         destLongitude = intent.getExtras().getDouble("destLongitude");
         destLatitude = intent.getExtras().getDouble("destLatitude");
+//        startLongitude = intent.getExtras().getDouble("startLongitude");
+//        startLatitude = intent.getExtras().getDouble("startLatitude");
         //stationName=intent.getExtras().getString("stationName");
         TMapPoint mytMapPoint = new TMapPoint(myLatitude,myLongitude);// 마커 놓을 좌표 (위도, 경도 순서)
         TMapPoint desttMappoint = new TMapPoint(destLatitude,destLongitude); // 마커 놓을 좌표 (위도, 경도 순서)
@@ -86,7 +110,7 @@ public class Marker extends AppCompatActivity {
         TMapPolyLine tpolyline = new TMapPolyLine();
         tpolyline.setLineColor(Color.BLUE);
         tpolyline.setLineWidth(2);
-        ArrayList<String> pathData = new ArrayList<String>();
+
         pathData = intent.getExtras().getStringArrayList("pathData");
         for(int i=0;i<pathData.size();i++){
             double tempLat, tempLong;
@@ -98,14 +122,40 @@ public class Marker extends AppCompatActivity {
             Log.e("test", tempA[0]);
             TMapPoint temp = new TMapPoint(tempLat, tempLong);
             tpolyline.addLinePoint(temp);
+
+            Location locationA = new Location("point A");
+            locationA.setLatitude(myLatitude);
+            locationA.setLongitude(myLongitude);
+            Location locationB = new Location("point B");
+            locationB.setLatitude(tempLat);
+            locationB.setLongitude(tempLong);
+
+            distance = Double.valueOf(locationA.distanceTo(locationB));
+            Log.d(i+"번째 distance 거리",String.valueOf(distance)+"m");
+            dist = new String[String.valueOf(distance).length()];
+            dist[i] = String.valueOf(distance);
+            Log.d(i+"번째 dist 거리",String.valueOf(dist[i])+"m");
+//            min=dist[0];
+//            Log.d("min거리",String.valueOf(min)+"m");
+
         }
+        for(int i=0; i<pathData.size(); i++){
+            Log.d("dist 거리",String.valueOf(dist[i])+"m");
+        }
+
+//        for(int i=0; i<dist.length; i++){
+//            if(min>dist[i]){
+//                min=dist[i];
+//            }
+//        }
+//        Log.d("내위치 사이 거리: ",min+"m");
         tmapview.addTMapPolyLine("path", tpolyline);
 
         relativeLayout.addView(tmapview);
         setContentView(relativeLayout);
+
+
+
     }
-
-
-
 
 }
