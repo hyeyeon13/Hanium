3bafa2fdce94b1b611f5026b3555f208d7bdf09e
junmin <wnsals1716@gmail.com>
2020-10-19 22:16:27 +0900

    소민 -- relative layout 안에 위도, 경도, 고도 지도 위에 띄우기 진행중... relative layout 생성 예정

--- app/src/main/java/org/techtown/hanium/Marker.java
+++ app/src/main/java/org/techtown/hanium/Marker.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 
 public class Marker extends AppCompatActivity {
     ArrayList<String> pathData = new ArrayList<String>();
-    Double myLongitude, myLatitude;
+    Double myLongitude, myLatitude, myAltitude;
     Double destLongitude, destLatitude;
     String min;
     Double num_min;
@@ -37,8 +37,40 @@ public class Marker extends AppCompatActivity {
     String stationName;
     double latitude = 0;
     double longitude = 0;
+    double altitude = 0;
     //경도 : longitude 범위 : 127
     //위도 : latitude 범위 : 37
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            double altitude = location.getAltitude();   //고도
+            float accuracy = location.getAccuracy();    //정확도
+            String provider = location.getProvider();   //위치제공자
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,6 +119,7 @@ public class Marker extends AppCompatActivity {
         tmapview.setCenterPoint( centerLong, centerLat ); //지도의 중심지점 좌표 (경도, 위도 순서)
         Log.d("내 위도  ", String.valueOf(myLatitude));
         Log.d("내 경도  ", String.valueOf(myLongitude));
+        Log.d("내 고도  ", String.valueOf(myAltitude));
         Log.d("목적지 위도  ", String.valueOf(destLatitude));
         Log.d("목적지 경도  ", String.valueOf(destLongitude));
 
@@ -146,6 +179,7 @@ public class Marker extends AppCompatActivity {
             alertDialog.show();
         }
         relativeLayout.addView(tmapview);
+        //relativeLayout.addView(infoview);
 
         setContentView(relativeLayout);
 
@@ -161,6 +195,8 @@ public class Marker extends AppCompatActivity {
 //                }
                 TMapPoint  tp= new TMapPoint(latitude,longitude);
                 Log.d("테스트", tp.toString() );
+                double altitude = location.getAltitude();
+
 
 
                 Double desDist;
