6c112712febfa84a41c4ce3cfb896ec8e77f7be6
dylee(desk-top) <speed1252@gmail.com>
2020-10-23 21:33:06 +0900

    즐찾추가

--- app/src/main/java/org/techtown/hanium/MainActivity.java
+++ app/src/main/java/org/techtown/hanium/MainActivity.java
@@ -27,10 +27,21 @@ import org.w3c.dom.Text;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-    public String login_id;
     TextView people1;
     TextView people2;
+    TextView favor1;
+    TextView favor2;
+    TextView favor3;
+
+    Button btnfavor1;
+    Button btnfavor2;
+    Button btnfavor3;
+
+    public String login_id;
+    public String px1, py1, px2, py2, px3, py3;
     public people_list task;
+    public favor_list favortastk;
+
 
 
     @Override
@@ -38,61 +49,81 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        people1 = (TextView) findViewById(R.id.people1);
-        people2 = (TextView) findViewById(R.id.people2);
+        people1 = (TextView)findViewById(R.id.people1);
+        people2 = (TextView)findViewById(R.id.people2);
+
+        favor1 = (TextView)findViewById(R.id.favor1);
+        favor2 = (TextView)findViewById(R.id.favor2);
+        favor3 = (TextView)findViewById(R.id.favor3);
+
+        btnfavor1 = (Button)findViewById(R.id.btnfavor1);
+        btnfavor2 = (Button)findViewById(R.id.btnfavor2);
+        btnfavor3 = (Button)findViewById(R.id.btnfavor3);
+
 
         Intent intent = getIntent();
         login_id = intent.getExtras().getString("log_ok_id");
+        Log.d("아이디체크", login_id);
 
 
-        Button button2 = findViewById(R.id.favorite);
-        button2.setOnClickListener(new View.OnClickListener() {
+        btnfavor1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Favorites.class);
+                Intent intent=new Intent(getApplicationContext(),Favorites.class);
+                intent.putExtra("log_ok_id", login_id);
+                intent.putExtra("chkid", "1");
                 startActivity(intent);
             }
         });
-        Button pathSetting = (Button) findViewById(R.id.pathsetting);
+        btnfavor2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Favorites.class);
+                intent.putExtra("log_ok_id", login_id);
+                intent.putExtra("chkid", "2");
+                startActivity(intent);
+            }
+        });
+        btnfavor3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Favorites.class);
+                intent.putExtra("log_ok_id", login_id);
+                intent.putExtra("chkid", "3");
+                startActivity(intent);
+            }
+        });
+        Button pathSetting=(Button)findViewById(R.id.pathsetting);
         pathSetting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), select_path.class);
+                Intent intent=new Intent(getApplicationContext(),select_path.class);
                 startActivity(intent);
             }
         });
-        Button button9 = (Button) findViewById(R.id.guardian);
+        Button button9=(Button)findViewById(R.id.guardian);
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), People.class);
+                Intent intent=new Intent(getApplicationContext(),People.class);
                 intent.putExtra("log_ok_id", login_id);
                 startActivity(intent);
             }
         });
 
         getpeople_list();
+        getfavor_list();
         RelativeLayout relativeLayout = new RelativeLayout(this);
         TMapView tmapview = new TMapView(this);
         tmapview.setSKTMapApiKey("l7xxa9511b15f91f4c3e97455a7a1ac155d2");
 
-        TextView textView1 = findViewById(R.id.textview4);
-        SpannableString content = new SpannableString("Content");
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        textView1.setText(content);
+//        SpannableString content = new SpannableString("Content");
+//        content.setSpan(new UnderlineSpan(), 0, content.length(), 0); textView1.setText(content);
 
-        textView1.setClickable(false);
-        textView1.setFocusable(false);
-        TextView textView2 = findViewById(R.id.editText6);
-        textView2.setClickable(false);
-        textView2.setFocusable(false);
-        TextView textView3 = findViewById(R.id.editText5);
-        textView3.setClickable(false);
-        textView3.setFocusable(false);
 
-        //  TMapTapi tMapTapi = new TMapTapi(this);
+      //  TMapTapi tMapTapi = new TMapTapi(this);
         //relativeLayout.addView(tmapview);
-        //  setContentView(relativeLayout);
+      //  setContentView(relativeLayout);
     }
 
     private class people_list extends AsyncTask<String, Void, String> {
@@ -164,18 +195,123 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("getpeople", "가져오기");
 
-        if (people_r.equals("0") == true) Log.d("보호자없음", "등록된 보호자가 없습니다.");
-        else if (people_r.equals("-2") == true) Log.d("네트워크", "네트워크에러임");
-        else {
+        if(people_r.equals("0") == true) Log.d("보호자없음", "등록된 보호자가 없습니다.");
+        else if(people_r.equals("-2") == true) Log.d("네트워크", "네트워크에러임");
+        else
+        {
             people_array = people_r.split("@");
             Log.d("네트워크2", people_r);
-            if (people_array.length > 0) people1.setText(people_array[0]);
-            if (people_array.length > 1) people2.setText(people_array[1]);
+            if(people_array.length > 0) people1.setText(people_array[0]);
+            if(people_array.length > 1) people2.setText(people_array[1]);
         }
 
     }
 
-    public String server_network_check(String host) {
+    private class favor_list extends AsyncTask<String, Void, String> {
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            String id = arg0[0];
+            try {
+                String link = "http://211.221.215.166/androidwithdb/favorites_list.php?ID=" + id;
+
+                String ret = server_network_check(link);
+                if (ret.equals("1") != true) return ret;
+
+                HttpURLConnection urlConn = null;
+                URL url = new URL(link);
+                urlConn = (HttpURLConnection) url.openConnection();
+                urlConn.setConnectTimeout(1000);
+                urlConn.setRequestMethod("POST"); // URL 요청에 대한 메소드 설정 : POST.
+                urlConn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
+                urlConn.setRequestProperty("Context_Type", "application/x-www-form-urlencoded;cahrset=UTF-8");
+
+
+                if (urlConn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    Log.d("retret2", "" + ret);
+                    return "-2";
+                }
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConn.getInputStream(), "UTF-8"));
+
+                StringBuffer sb = new StringBuffer("");
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    break;
+                }
+                reader.close();
+                if (urlConn != null) urlConn.disconnect();
+
+                Log.d("라인값", sb.toString());
+
+                return sb.toString();
+            } catch (Exception e) {
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
+    private void getfavor_list()
+    {
+        String favor_r = null;
+        String[] favor_array;
+
+        favortastk = new favor_list();
+        try {
+            favor_r = favortastk.execute(login_id).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d("getfavor", "가져오기");
+
+        if(favor_r.equals("0") == true) Log.d("즐겨찾기 없음", "등록된 보호자가 없습니다.");
+        else if(favor_r.equals("-2") == true) Log.d("네트워크", "네트워크에러임");
+        else
+        {
+            favor_array = favor_r.split("@");
+            Log.d("네트워크2", favor_array[0]);
+            for(int i=0; i<favor_array.length/4; i++)
+            {
+                Log.d("테스트", favor_array[i*4]);
+                if(favor_array[i*4].equals("1"))
+                {
+                    Log.d("favor1", favor_array[i*4+1]);
+                    favor1.setText(favor_array[i*4+1]);
+                    px1 = favor_array[i*4+2];
+                    py1 = favor_array[i*4+3];
+                }
+                else if(favor_array[i*4].equals("2"))
+                {
+                    Log.d("favor2", favor_array[i*4+1]);
+                    favor2.setText(favor_array[i*4+1]);
+                    px2 = favor_array[i*4+2];
+                    py2 = favor_array[i*4+3];
+                }
+                else
+                {
+                    Log.d("favor3", favor_array[i*4+1]);
+                    favor3.setText(favor_array[i*4+1]);
+                    px3 = favor_array[i*4+2];
+                    py3 = favor_array[i*4+3];
+                }
+            }
+        }
+
+    }
+
+    public String server_network_check (String host){
         return "1";
     }
 }
